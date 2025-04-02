1aba6eadca3a03857396fc878075ce51879d1b9a
Kumar4nil15 <Anilkdyadav15@gmail.com>
2025-04-02 09:28:53 +0530

    Update result_page.py

--- Framework/pages/result_page.py
+++ Framework/pages/result_page.py
@@ -1,1 +1,8 @@
+from selenium.webdriver.common.by import By
 
+class ResultPage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def get_result(self):
+        return self.driver.find_element(By.ID, "result").text
