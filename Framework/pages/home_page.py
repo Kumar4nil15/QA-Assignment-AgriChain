d12d26b33a179b6d1e912aed8987bc047071887f
Kumar4nil15 <Anilkdyadav15@gmail.com>
2025-04-02 09:27:57 +0530

    Update home_page.py

--- Framework/pages/home_page.py
+++ Framework/pages/home_page.py
@@ -1,1 +1,11 @@
+from selenium.webdriver.common.by import By
 
+class HomePage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def enter_string(self, input_text):
+        self.driver.find_element(By.ID, "inputField").send_keys(input_text)
+
+    def click_submit(self):
+        self.driver.find_element(By.ID, "submitButton").click()
