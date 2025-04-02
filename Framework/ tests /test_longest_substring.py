bef00d88e0abd15668bd21775f26b0d84e995453
Kumar4nil15 <Anilkdyadav15@gmail.com>
2025-04-02 09:30:14 +0530

    Update test_longest_substring.py

--- Framework/ tests /test_longest_substring.py
+++ Framework/ tests /test_longest_substring.py
@@ -1,1 +1,21 @@
+import pytest
+from utils.browser_setup import get_driver
+from pages.home_page import HomePage
+from pages.result_page import ResultPage
 
+@pytest.fixture
+def setup():
+    driver = get_driver()
+    driver.get("https://agrichain.com")
+    yield driver
+    driver.quit()
+
+def test_longest_substring(setup):
+    driver = setup
+    home_page = HomePage(driver)
+    result_page = ResultPage(driver)
+
+    home_page.enter_string("abcabcbb")
+    home_page.click_submit()
+    
+    assert result_page.get_result() == "3", "Test Failed: Expected output is 3"
